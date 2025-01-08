0b450e355fbe2f7b3d58cd60603487723aab7f17
unknown <ndiaye.khady30@ugb.edu.sn>
2025-01-08 19:28:27 +0100

    Creation entite et configuration JPA avec mysql

--- src/main/java/sn/edu/ugb/ipsl/git/HelloSpringBoot/entities/Etudiant.java
+++ src/main/java/sn/edu/ugb/ipsl/git/HelloSpringBoot/entities/Etudiant.java
@@ -1,5 +1,7 @@
 package sn.edu.ugb.ipsl.git.HelloSpringBoot.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +11,10 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Etudiant {
+    @Id
+    private Integer id;
     private String nom;
     private String prenom;
     private String adresse;
